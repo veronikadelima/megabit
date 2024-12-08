61963ea702b7d6eca22d1160d3b6de0c463b1d92
veronikadelima <veronikadelima@gmail.com>
2024-12-08 17:00:13 -0300

    first commit

---
+++ src/components/header/page.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="container m-auto flex flex-col items-center justify-center h-1/6 absolute">
+      <img src="/images/megabit-logo.png" className="w-60" alt="Imagem da Logomarca - Mega Bit"/>
+    </div>
+  );
+}
---
+++ src/components/navbar/page.tsx
@@ -0,0 +1,31 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+export const Navbar = () => {
+  return (
+    <div className="bg-slate-900 w-20 relative">
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+    </div>
+  );
+}
